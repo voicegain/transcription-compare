c7add74cedac8ebd4d7106ed0bc6fa734c2618ba
HannaHUp <huishenzhan@gmail.com>
2019-07-26 15:09:36 -0500

    plot the distribution

--- transcription_compare/utils/plot_util.py
+++ transcription_compare/utils/plot_util.py
@@ -1,9 +1,36 @@
 import matplotlib.pyplot as plt
 import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from scipy.stats import *
+import scipy.stats as stats
 
 
-def t(distance_list):
-    pass
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def t(distance_list, to_edit_step, to_edit_width):
+    mean_distance = np.mean(distance_list)
+    sns.set_style("ticks")
+    w, p = stats.shapiro(distance_list)  # we will reject the hypothesis if p <0.05. X is not normal distribution.
+    plt.figure(figsize=(8, 4), dpi=100)
+    sns.distplot(distance_list, hist=True, kde=True, rug=True, fit=norm,
+                 kde_kws={"color": "lightcoral", "lw": 1.5, 'linestyle': '--'},
+                 rug_kws={'color': 'lightcoral', 'alpha': 1, 'lw': 2, }, label='distance')
+
+    plt.axvline(mean_distance, color='lightcoral', linestyle=":", alpha=2)
+    plt.text(mean_distance + 2, 0.012, 'distance mean: %.1fcm' % (mean_distance), color='indianred')
+    plt.annotate(s='We are using The Shapiro-Wilk test'.format(p), xy=(max(distance_list) - 3, 0.055))
+    plt.annotate(s='p_value{} '.format(p), xy=(max(distance_list) - 3, 0.05))
+    plt.annotate(s='w{} '.format(w), xy=(max(distance_list) - 3, 0.045))
+    plt.annotate(s='the closer of w to 1,\n the better the normal distribution fits', xy=(max(distance_list) - 3, 0.04))
+    if p > 0.05:
+        plt.annotate(s='it is a normal distribution', xy=(max(distance_list) - 3, 0.035))
+    plt.grid(linestyle='--')
+    plt.title("distribution_of_distance step{} width{}".format(to_edit_step, to_edit_width))
+    plt.savefig("distribution_of_distance")
 
 
 def plot_alignment_result(alignment_result, to_edit_width, to_edit_step, file_name, file_path):
@@ -13,7 +40,13 @@ def plot_alignment_result(alignment_result, to_edit_width, to_edit_step, file_na
     i_list, distance_list, substitution_list, deletion_list, insertion_list = \
         _prepare_plot(output_list, to_edit_step)
 
-    t(distance_list)
+    # data_frame = pd.DataFrame({'step': i_list, 'distance': distance_list,
+    #                           'substitution': substitution_list,
+    #                            'deletion': deletion_list,
+    #                            'insertion': insertion_list})
+    # data_frame = pd.DataFrame({'step': i_list, 'distance': distance_list})
+    # data_frame.to_csv("distance.csv", index=False, sep=',')
+    t(distance_list, to_edit_step, to_edit_width)
 
     print('distance', distance_list)
     print('deletion_list', deletion_list)
