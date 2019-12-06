edfe68e063c6b5bf0849b2df8b2d33836c80424f
HannaHUp <huishenzhan@gmail.com>
2019-12-06 15:48:54 -0600

    add more packages in packages

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 # cli
-REQUIRES = ["click", "inflect"]
+REQUIRES = ["click", "inflect", "tqdm", "nltk", "enum", "pkg_resources", "metaphone", "bs4", "requests", "matplotlib", "seaborn"]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="transcription_compare",
-    version="0.0.4",
+    version="0.0.5",
     description="Compare transcription",
     long_description=long_description,
     long_description_content_type="text/markdown",
