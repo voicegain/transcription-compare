ed86bd83e68f13bd7906d4722b3ad782c0295f96
HannaHUp <huishenzhan@gmail.com>
2020-10-27 16:26:54 -0500

    include wiki wordnet csv

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="transcription_compare",
-    version="0.0.13",
+    version="0.0.14",
     description="Compare transcription",
     long_description=long_description,
     long_description_content_type="text/markdown",
